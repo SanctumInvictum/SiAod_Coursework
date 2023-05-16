c1b2dd44b4582756a1863a0007932e9c84bbbdf3
SanctumInvictum <yura.sokolov.03@gmail.com>
2023-05-16 16:35:13 +0300

    Заготовочка

--- main.py
+++ main.py
@@ -2,6 +2,11 @@ import math
 import csv
 import numpy as np
 
+
+"""
+Функция для чтения csv файла из таблицы.
+Преобразует файл в матрицу представленную двумерным
+"""
 def ReadCSV(file):
     CSVarr = []
     with open(file, newline='', encoding='utf-8') as csvfile:
@@ -10,14 +15,39 @@ def ReadCSV(file):
             CSVarr.append(row)
             print(', '.join(row))
         return CSVarr
+
+
+def PrintTable():
+    pass
+
+
 def sort():
     pass
+
 
 def search():
     pass
 
-def CreateReport(data):
+
+def Validator():
     pass
+
+
+def totalRevenue(data):
+    pass
+
+
+def ProductShare(data):
+    pass
+
+
+def CreateReport(data):
+    print("Общая выручка магазина составила:\n", totalRevenue(data))
+    print("товар, который был продан наибольшее кол-во раз:\n")
+    print("Товар, который принес наибольшую выручку:\n")
+    PrintTable(data)
+    ProductShare(data)
+
 
 if __name__ == '__main__':
     data = ReadCSV('table.csv')
