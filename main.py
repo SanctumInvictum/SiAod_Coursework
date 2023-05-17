3201b848675f6d0e5ce080a3acb1661e22833170
SanctumInvictum <yura.sokolov.03@gmail.com>
2023-05-17 23:38:07 +0300

    1.0 stable version

--- main.py
+++ main.py
@@ -1,30 +1,63 @@
-import math
 import csv
-import numpy as np
-import heapq
 from datetime import datetime, date
+from tabulate import tabulate
 
 
-class Order:
-    def __init__(self):
-        self.Order = []
+class Node:
+    """
+    Реализация структуры данных узла для структуры хэш-таблицы
+    """
+    def __init__(self, key, value):
+        self.key = key
+        self.value = value
+        self.next = None
 
 
-"""
-Функция для чтения csv файла из таблицы.
-Преобразует файл в двумерный массив, где каждый одномерный массив
-является строкой из файла
-"""
+class HashTable:
+    """
+    Структура данных - хэш таблица с рехешированием через метод цепочек
+    """
+    def __init__(self, capacity):
+        self.capacity = capacity
+        self.size = 0
+        self.buckets = [None] * self.capacity
+
+    def insert(self, key, value):
+        index = hash(key) % self.capacity
+        node = self.buckets[index]
+        while node:
+            if node.key == key:
+                node.value = value
+                return
+            node = node.next
+        new_node = Node(key, value)
+        new_node.next = self.buckets[index]
+        self.buckets[index] = new_node
+
+    def get(self, key):
+        index = hash(key) % self.capacity
+        node = self.buckets[index]
+        while node:
+            if node.key == key:
+                return node.value
+            node = node.next
+        return None
+
+
 def read_csv(file):
+    """
+    Функция для чтения csv файла из таблицы.
+    Преобразует файл в двумерный массив, где каждый одномерный массив
+    является строкой из файла
+    """
     try:
         with open(file, newline='', encoding='utf-8') as csv_file:
             reader = csv.reader(csv_file, delimiter=';', quotechar='|')
             csv_arr = []
-            isFirst = True
+            is_first = True
             for row in reader:
-                if isFirst:
-                    isFirst = False
-                    csv_arr.append(row)
+                if is_first:
+                    is_first = False
                     continue
                 validate(row)
                 csv_arr.append(row)
@@ -33,80 +66,15 @@ def read_csv(file):
         print("Файл не открывается")
 
 
-def partition(array, left, right):
-    pivot = array[right]
-    i = left
-    j = right - 1
-    while i < j:
-        while (array[i] < pivot and i < right):
-            i += 1
-        while (array[j] >= pivot and j > left):
-            j -= 1
-        if i < j:
-            array[i], array[j] = array[j], array[i]
-            i += 1
-            j -= 1
-    if array[i] > pivot:
-        array[i], array[right] = array[right], array[i]
-    return i
-
-
-def quick_sort(array, start, end):
-    if start < end:
-        right_start = partition(array, start, end)
-        quick_sort(array, start, right_start - 1)
-        quick_sort(array, right_start + 1 , end)
-
-"""
-Функция запускающая сортировку для массива, сформированного по заданному
-параметру, вовзращает отсортированный массив содержащий данные заданного параметра
-"""
-def sort_param(data, param):
-    sort_arr = []
-    for i in range(1, len(data)):
-        sort_arr.append(int(data[i][param]))
-    quick_sort(sort_arr, 0, len(sort_arr)-1)
-    return sort_arr
-
-
-def binary_search(arr, element):
-    # обозначим элементы между которыми находится искомый
-    first = 0
-    last = len(arr)-1
-    # Сдвигаем границы для поиска
-    while first <= last:
-        # Делим пополам
-        mid = int((first + last)/2)
-        # Условие выхода
-        if arr[mid] == element:
-            return mid
-        # Если меньше среднего, сдвигаем правую границу
-        elif element < arr[mid] :
-            last = mid-1
-        # Иначе левую
-        else:
-            first = mid+1
-    return None
-
-
-
-def search_param(data, element, param, param_index):
-    result = []
-    for i in range(1, len(data)):
-        if data[i][param_index] == param:
-            result.append(data[i][element])
-    return result
-
-
-"""
-Функция проверки данных на валидность: 
-- Итоговая сумма за все проданные единицы товара должна быть ровна произведению
-- Дата заказа не может бфть позже текущей 
-- Недопустимо отрицательное или нулевое кол-во товаров
-- Недопустима нулевая или отрицательная цена за единицу товара или за весь
-- преобразует строковые числа из файла в int тем самым проверяя их на тип
-"""
 def validate(arr):
+    """
+    Функция проверки данных на валидность:
+    - Итоговая сумма за все проданные единицы товара должна быть ровна произведению
+    - Дата заказа не может бфть позже текущей
+    - Недопустимо отрицательное или нулевое кол-во товаров
+    - Недопустима нулевая или отрицательная цена за единицу товара или за весь
+    - преобразует строковые числа из файла в int тем самым проверяя их на тип
+    """
     arr[0], arr[4], arr[5], arr[6] = int(arr[0]), int(arr[4]), int(arr[5]), int(arr[6])
     if arr[4] <= 0:
         raise Exception("Отрицательное или нулевое кол-во товара")
@@ -121,44 +89,115 @@ def validate(arr):
         raise Exception("Дата добавления превышает текущую")
 
 
-def total_revenue(data):
+def partition(array, left, right, param):
+    """
+    Часть алгоритма быстрой сортировки Хоара
+    Выделяет опорный элемент - pivot и по нему пересобирает массив в следующем виде:
+    элементы меньше опорного, опорный, элементы больше опорного
+    """
+    pivot = array[right][param]
+    i = left
+    j = right - 1
+    while i < j:
+        while array[i][param] < pivot and i < right:
+            i += 1
+        while array[j][param] >= pivot and j > left:
+            j -= 1
+        if i < j:
+            array[i], array[j] = array[j], array[i]
+            i += 1
+            j -= 1
+    if array[i][param] > pivot:
+        array[i], array[right] = array[right], array[i]
+    return i
+
+
+def quick_sort(array, start, end, param):
+    """
+    Основная функция быстрой сортировки Хоара. Вызывает partition разделяя массив по опорному элементу,
+    затем рекурсивно вызывает себя дважды для элементов слева от опорного и справа от опорного, пока не распределит
+    все элементы в правильном порядке
+    """
+    if start < end:
+        right_start = partition(array, start, end, param)
+        quick_sort(array, start, right_start - 1, param)
+        quick_sort(array, right_start + 1, end, param)
+
+
+def sort_param(data, param):
+    """
+    Функция вызывающая сортировку для массива по заданному параметру и возвращающая отсортированную копию данных
+    параметр:
+    - 0 номер заказа
+    - 4 кол-во продаж
+    - 5 цена за единицу
+    - 6 общая стоимость
+    """
+    sort_arr = data
+    quick_sort(sort_arr, 0, len(sort_arr)-1, param)
+    return sort_arr
+
+
+def revenue(data, param):
+    """
+    Функция, суммирующая элементы по параметру
+    """
     total_sum = 0
-    for i in range(1, len(data)):
-        total_sum += int(data[i][6])
+    for i in range(len(data)):
+        total_sum += int(data[i][param])
     return total_sum
 
 
 def best_product(data, param):
-    arr = sort_param(data, param)
-    max_amount = arr[-1]
-    return search_param(data, 2, max_amount, param)
+    """
+    Функция, вызывающая сортировку по параметру и возвращающая имя по наибольшему значению параметра
+    """
+    return sort_param(data, param)[-1][2]
 
 
-def best_total(arr, param):
-    #data = {}
-    #data['key'] = value
-    pass
+def print_table(data, total_table, amount_table):
+    """
+    Функция печати аккуратной таблицы, принимает наименования товаров из данных, хэш-таблицу долей
+    и хэш-таблицу количетсва проданного товара и через хэш-поиск по ключу-наименованию товара добавляет в итоговую
+    таблицу соответствующие значения кол-ва проданного товара и его доли от общей прибыли
+    """
+    table = [['Название товара', 'Количество продаж', 'Доля товара, %']]
+    for row in data:
+        table.append([row[2], amount_table.get(row[2]), round(total_table.get(row[2]),3)])
 
-def print_table(arr):
-    pass
+    print(tabulate(table, headers='firstrow', tablefmt='fancy_grid'))
 
 
-def product_share(data):
-    pass
+def product_share(data, venue):
+    """
+    Функция, формирующая хэш-таблицу со связью наименований товаров с их долей в процентах от общей суммы продаж
+    """
+    arr = list(reversed(data))
+    table = HashTable(len(arr))
+    for i in range(len(arr)):
+        table.insert(arr[i][2], arr[i][6]/(venue/100))
+    return table
 
 
 def create_report(data):
-    print("Общая выручка магазина составила:\n", total_revenue(data))
-    print("товар(ы), который/ые был(и) продан(ы) наибольшее кол-во раз:\n", best_product(data, 4))
-    print("Товар(ы), который/ые принес(ли) наибольшую выручку:\n", best_product(data, 6))
-    print_table(data)
-    product_share(data)
+    """
+    Функция, формирующая отчет по всем требуемым заданиям
+    """
+    total_revenue = revenue(data, 6)
+    best_amount = best_product(data, 4)
+    best_total = best_product(data, 6)
+    print("Общая выручка магазина составила:\n", total_revenue,"у.е.\n-------------------------------------")
+    print("товар, проданный наибольшее кол-во раз:\n", best_amount, "\n-------------------------------------")
+    print("Товар, принесший наибольшую выручку:\n", best_total, "\n-------------------------------------")
+    amount_table = HashTable(len(data))
+    for i in range(len(data)):
+        amount_table.insert(data[i][2], data[i][4])
+    print_table(data, product_share(sort_param(data, 6), total_revenue), amount_table)
 
 
 if __name__ == '__main__':
     data = read_csv('table.csv')
     create_report(data)
-    #print(np.array(data))
     for i in range(len(data)):
         print(data[i])
 
